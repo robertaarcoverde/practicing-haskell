3df1b722909b79e367cbd9af733550b2aa2674a6
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-09-01 23:21:58 -0300

    adding some examples

--- functions.hs
+++ functions.hs
@@ -32,6 +32,8 @@ comb _ 0  = [[]]
 comb [] _ = []
 comb (x:xs) n = map (x:) (comb xs (n-1)) ++ comb xs n
 
+-- example: comb [1..4] 2 = [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+
 
 {------------------------------------------------------------------------------
 	powerset function
@@ -45,6 +47,7 @@ powerset (x:xs) = ss ++ (map (x:) ss)
 -- powerset of a list is the powerset of that same list minus the head, combined with the head in all possible ways
 -- for ex, powerset "abc" = powerset "bc" ++ "a" combined with each element in powerset "bc"
 
+-- example: powerset "abc" = ["","c","b","bc","a","ac","ab","abc"]
 
 {------------------------------------------------------------------------------
 	partitions function
@@ -55,6 +58,9 @@ part n l
  | (n < 0) = []
  | (n == 0) = [[]]
  | otherwise = [ r | l' <- [l, (l-1)..1], r <- map (l':) (part (n - l') l')]
+
+
+-- example: part 5 3 = [[3,2],[3,1,1],[2,2,1],[2,1,1,1],[1,1,1,1,1]]
 
 
 {------------------------------------------------------------------------------
@@ -68,3 +74,4 @@ permutations (xs) = [ r | z <- xs, r <- map (z:) (permutations (remove z xs))]
 remove :: Eq a => a -> [a] -> [a]
 remove x (y:ys) = if (x == y) then ys else y:(remove x ys)
 
+-- example: permutations [1,2,3] = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
