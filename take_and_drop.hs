ed99250d473635c8f594aa792f06093939ad2f43
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-09-16 08:49:37 -0300

    results report after executing the new definition of drop and take

--- take_and_drop.hs
+++ take_and_drop.hs
@@ -6,24 +6,37 @@
 	take' function
  ------------------------------------------------------------------------------}
 
-take' 0 xs = []
+take' :: Integer -> [a] -> [a]
 take' n [] = []
+take' 0 xs = []
 take' n (x:xs) = x : take' (n-1) xs
 
 {------------------------------------------------------------------------------
 	drop' function
  ------------------------------------------------------------------------------}
 
-drop' 0 l = l
 drop' n [] = []
+drop' 0 l = l
 drop' n (x:xs) = drop' (n - 1) xs
+
+{------------------------------------------------------------------------------
+	ao usar drop n de uma lista infinita, o programa entra em loop
+	imprimindo os elementos da lista até que seja interrompido.
+
+		Ex: drop 3 [1..] resulta na impressão de [4,5,6,7,8...120382] 
+			(considerando que a execução foi interrompida neste ponto)
+			
+	usando a redefinição de drop, com as primeiras duas expressões trocadas,
+	obtive o mesmo resultado: funciona normalmente pra listas finitas,
+	e imprime até ser interrompido quando aplicado a listas infinitas.
+ ------------------------------------------------------------------------------}
 
 {------------------------------------------------------------------------------
  | 2) Extending Permutations' list-comprehension expressions
  ------------------------------------------------------------------------------}
 
 {------------------------------------------------------------------------------
-	original permutations function
+	permutations function
  ------------------------------------------------------------------------------}
 
 permutations [] = [[]]
