0aa5eae9a1bd2b2ff61429b1824203eb8d198d55
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-10-03 23:38:57 -0300

    implementing member function

--- red_black_tree.hs
+++ red_black_tree.hs
@@ -3,5 +3,23 @@
 	features.
  ------------------------------------------------------------------------------}
 
- data Color = R | B
- data Tree a = E | T Color (Tree a) a (Tree a)
+data Color = R | B deriving Show
+data Tree a = E | T Color (Tree a) a (Tree a) deriving Show
+
+{-----------------------------------------------------------------------------
+indicates whether a given value is a member of the red-black tree
+
+	tests:
+		member E 1 :: False
+		member (T R (E) 2 (E)) 1 :: False
+		member (T R (E) 1 (E)) 1 :: True
+		member (T R E 1 (T B E 2 E)) 2 :: True
+	
+------------------------------------------------------------------------------}
+member :: Ord a => Tree a -> a -> Bool
+member E _ = False
+member (T _ t1 v t2) x
+		| (v == x) = True
+		| (v > x) = member t1 x
+		| otherwise = member t2 x
+		
