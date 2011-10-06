73009d1a72a697c74489cf8de818a41814c8d911
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-10-05 23:09:41 -0300

    done with the balance function

--- red_black_tree.hs
+++ red_black_tree.hs
@@ -34,6 +34,7 @@ insert t x = turnBlack (ins t x)
 balance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
 balance B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
 balance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
+balance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
 balance c t1 x t2 = T c t1 x t2 -- caso base
 
 {------------------------------------------------------------------------------
