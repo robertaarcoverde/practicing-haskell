c2dd40468cd9d3667efc6f752c6cb63a87d137ac
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-10-05 23:52:56 -0300

    adding leftist heap impl

---
+++ leftist_heap.hs
@@ -0,0 +1,27 @@
+{------------------------------------------------------------------------------
+ 
+Leftist Heap
+
+------------------------------------------------------------------------------}
+
+data Heap a = E | T Int a (Heap a) (Heap a) deriving Show
+
+merge :: Ord a => Heap a -> Heap a -> Heap a
+merge E t = t
+merge t E = t
+merge t1@(T r1 v1 a1 b1) t2@(T r2 v2 a2 b2)
+	| (v1 < v2) = makeT v1 a1 (merge b1 t2)
+	| otherwise = makeT v2 a2 (merge b2 t1)
+	where
+		makeT v t1 t2 = if rank t1 >= rank t2 then T (rank t2 + 1) v t1 t2
+			else T (rank t1 + 1) v t2 t1
+
+rank E = 0
+rank (T r v1 a1 b1) = r
+
+insert :: Ord a => a -> Heap a -> Heap a
+insert v h = merge (T 1 v E E) h
+
+delete :: Ord a => Heap a -> Heap a
+delete E = E
+delete (T _ v1 h1 h2) = merge h1 h2
--- red_black_tree.hs
+++ red_black_tree.hs
@@ -35,43 +35,5 @@ balance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
 balance B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
 balance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
 balance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
-balance c t1 x t2 = T c t1 x t2 -- caso base
+balance c t1 x t2 = T c t1 x t2
 
-{------------------------------------------------------------------------------
- 
-data Color = R | B deriving Show
-data RB a = E | T Color (RB a) a (RB a) deriving Show
-
-insert :: Ord a => a -> RB a -> RB a
-insert x s =
-	T B a z b
-	where
-	T _ a z b = ins s
-	ins E = T R E x E
-	ins s@(T B a y b)
-		| x<y = balance (ins a) y b
-		| x>y = balance a y (ins b)
-		| otherwise = s
-	ins s@(T R a y b)
-		| x<y = T R (ins a) y b
-		| x>y = T R a y (ins b)
-		| otherwise = s
-
-member :: Ord a => a -> RB a -> Bool
-member x E = False
-member x (T _ a y b)
-	| x<y = member x a
-	| x>y = member x b
-	| otherwise = True
-
-{- balance: first equation is new,
-   to make it work with a weaker invariant -}
-balance :: RB a -> a -> RB a -> RB a
-balance (T R a x b) y (T R c z d) = T R (T B a x b) y (T B c z d)
-balance (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
-balance (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
-balance a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
-balance a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
-balance a x b = T B a x b
-
- ------------------------------------------------------------------------------}
