6f8c6b48e0a5e42f677c2aeec48553a26c1bf464
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-10-12 11:35:30 -0300

    defining equality and inequality on Set data types

--- type_classes.hs
+++ type_classes.hs
@@ -4,8 +4,11 @@ Type Classes - Defining Set
 
 ------------------------------------------------------------------------------}
 
-data Set a = Set [a]
+data Set a = Set [a] deriving Show
 
-instance Eq a => Eq Set a where
+instance Eq a => Eq (Set a) where
 	Set [] == Set [] = True
+	Set [] == Set (x:xs) = False
+	Set (x:xs) == Set [] = False
 	Set (x:xs) == Set (y:ys) = x == y && (xs == ys)
+	Set l1 /= Set l2 = not (l1 == l2)
