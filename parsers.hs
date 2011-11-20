2988999cb3dcb6f03085b145f1d282dea683f7bb
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-11-20 11:41:26 -0200

    Parser monad defined

---
+++ parsers.hs
@@ -0,0 +1,11 @@
+newtype Parser a = MkP (String -> [(a,String)])
+
+apply :: Parser a -> String -> [(a,String)]
+apply (MkP f) s = f s
+
+instance Monad Parser where
+    return x    = MkP f where f s = [(x,s)]
+    p >>= q     = MkP f
+        where f s = [(y,s'') | (x,s') <- apply p s, (y,s'') <- apply (q x) s']
+
+		
