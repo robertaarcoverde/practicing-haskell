9a5a22ff98b26cf63c2018931958163f80ced069
Roberta Arcoverde <roberta.arcoverde@gmail.com>
2011-11-20 12:12:11 -0200

    defining regexp data structure

--- parsers.hs
+++ parsers.hs
@@ -17,4 +17,23 @@ getchar = MkP f
 condchar :: (Char -> Bool) -> Parser Char
 condchar f = do { c <- getchar ; if (f c) then return c else fail "condition" }
 
-checkchar c = condchar (==c)		  
+checkchar c = condchar (==c)
+
+plus :: Parser a -> Parser a -> Parser a
+plus p1 p2 = MkP f
+    where f s = (apply p1 s) ++ (apply p2 s)
+
+orelse :: Parser a -> Parser a -> Parser a
+orelse p1 p2 = MkP f
+    where f s = let l = apply p1 s in
+                    if null l then apply p2 s
+                              else l
+
+data Exp = ExpChar Char
+    | ExpOr Exp Exp        
+    | ExpPlus Exp
+    | ExpSet (Char -> Bool)
+    | ExpSeq Exp Exp
+    | ExpDot							  
+	
+	
